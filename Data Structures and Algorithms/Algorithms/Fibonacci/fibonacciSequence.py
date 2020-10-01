0005db7e308a0a39da6add606ef4f9c32a4e16f7
Tahsin Ismail <tahsinismail69@gmail.com>
2020-10-01 16:29:25 +0600

    Fibonacci Sequence

---
+++ Data Structures and Algorithms/Algorithms/Fibonacci/fibonacciSequence.py
@@ -0,0 +1,31 @@
+#python3
+import time
+#Defining list with predefined fibonacci sequence
+sequence = [0,1]
+
+#Defining function to generate the sequence
+def fibonacciSequence(n):
+    #Start time of the function
+    start_time = time.time()
+    for i in range(n):
+        if i > 0 < n:
+            newSequence = sequence[i-1] + sequence[i]
+            sequence.append(newSequence)
+            
+    #Elapsed time per execution
+    elapsed_time = time.time() - start_time
+    
+    return "FibonacciSequence: \n\n{}  \n\nexecuted in : {:.7f}".format(sequence, elapsed_time)
+
+if __name__ == '__main__':
+
+    while True: 
+        try:
+            #Accepting only a postive integer
+            input_n = int(input("Enter a postive integer number: ")) 
+            assert(input_n  > 0)
+            break 
+        except:
+            print('Please enter a valid integer number\nand number have to be greater than 0')
+
+    print(fibonacciSequence(input_n))
